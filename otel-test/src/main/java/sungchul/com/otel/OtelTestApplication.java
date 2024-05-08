8d9a3b43b005a5c87aee6b6877c7cef91ebbeed1
kimsc1218 <kimsc1218@gmail.com>
2024-05-09 02:23:30 +0900

    자료 업로드

---
+++ otel-test/src/main/java/sungchul/com/otel/OtelTestApplication.java
@@ -0,0 +1,13 @@
+package sungchul.com.otel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtelTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtelTestApplication.class, args);
+    }
+
+}
