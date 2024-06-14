9e62d87639c56a3bafb2ab191b847355bb866968
kimsc1218 <kimsc1218@gmail.com>
2024-06-14 11:22:55 +0900

    2024.06.14
    - bat 파일명 수정 (run -> start)
    - nginx 종료 bat 파일 추가

--- application/all_run_server.bat
+++ application/all_run_server.bat
@@ -1,5 +1,5 @@
 @echo off
-start cmd /k "cd ./nginx && run_nginx.bat"
-start cmd /k "cd ./otel-collector && run_otelcol.bat"
-start cmd /k "cd ./otel-sdk-django-01 && run_django.bat"
-start cmd /k "cd ./otel-sdk-springboot-01 && run_springboot.bat"
+start cmd /k "cd ./nginx && start_nginx.bat"
+start cmd /k "cd ./otel-collector && start_otelcol.bat"
+start cmd /k "cd ./otel-sdk-django-01 && start_django.bat"
+start cmd /k "cd ./otel-sdk-springboot-01 && start_springboot.bat"
