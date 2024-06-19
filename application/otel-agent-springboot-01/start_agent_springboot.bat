7027d79f25b6c8983c3056424cf0aa0b860d340e
kimsc1218 <kimsc1218@gmail.com>
2024-06-20 01:47:35 +0900

    2024.06.20
    - agent방식 bat 실행 파일 추가
    - agent방식 프로젝트 내 설정 삭제
    - agent방식 프로젝트 내 라이브러리 삭제

--- application/otel-agent-springboot-01/start_agent_springboot.bat
+++ application/otel-agent-springboot-01/start_agent_springboot.bat
@@ -1,7 +1,8 @@
 @echo off
 rem Springboot build with gradle
+
 echo ## gradlew.bat assemble
-gradlew.bat assemble && cd .\build\libs && java -jar otel-agent-springboot-01-0.0.1-SNAPSHOT.jar
+gradlew.bat assemble&& cd .\build\libs&& set JAVA_TOOL_OPTIONS=-javaagent:..\..\opentelemetry-javaagent.jar&& set OTEL_METRIC_EXPORT_INTERVAL=1000&& set OTEL_TRACES_EXPORTER=otlp&& set OTEL_METRICS_EXPORTER=otlp&& set OTEL_LOGS_EXPORTER=otlp&& set OTEL_SERVICE_NAME =otel-agent-springboot-01&& set OTEL_EXPORTER_OTLP_ENDPOINT=http://127.0.0.1:9999&& set OTEL_EXPORTER_OTLP_TRACES_ENDPOINT=http://127.0.0.1:9999&& set OTEL_EXPORTER_OTLP_METRICS_ENDPOINT=http://127.0.0.1:9999&& set OTEL_EXPORTER_OTLP_LOGS_ENDPOINT=http://127.0.0.1:9999&& set OTEL_EXPORTER_OTLP_PROTOCOL=grpc&& java -jar otel-agent-springboot-01-0.0.1-SNAPSHOT.jar
 
 rem Run SpringBoot
-rem echo ## cd .\build\libs && java -jar otel-agent-springboot-01-0.0.1-SNAPSHOT.jar
+rem echo ## cd .\build\libs&& java -jar otel-agent-springboot-01-0.0.1-SNAPSHOT.jar
