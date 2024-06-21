dd82ebfd9fdb4a2eed4a51e11676105b10c79671
lee-jihyoun <lee.jihyoun7@gmail.com>
2024-06-21 18:03:26 +0900

    2024.06.21 - otel auto instrumentation minor change

--- application/otel-auto-django-01/otelautodjango01/settings.py
+++ application/otel-auto-django-01/otelautodjango01/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import os
-os.environ['OTEL_EXPORTER_OTLP_ENDPOINT'] = 'localhost:9999'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +25,7 @@ SECRET_KEY = 'django-insecure-)6es)rr%8*ibf_b07pz!)_nof^)xeb$+vh@c!*o%h3g_zvgyku
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
